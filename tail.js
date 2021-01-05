e0dc1a248200ea87f0c0c19ccc8e9d3141092af3
Idiljei <i.jeilani@gmail.com>
2021-01-05 16:33:58 +0000

    the last element of array

--- tail.js
+++ tail.js
@@ -1,4 +1,13 @@
+// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+
+  if ( actual === expected ) {
+   console.log(`Assertion Passed: ${actual} === ${expected}`); 
+
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+  
 };
  
 const tail = function(arr) {
