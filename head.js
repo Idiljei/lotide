071ddb9823ca1b7612709b7c1f033e652433dc33
Idiljei <i.jeilani@gmail.com>
2021-01-05 05:28:35 +0000

    Removing first array

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+};
+const head = function (array) {
+
+  return array[0] 
+
+  }
+
+
+
+// TEST CODE
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello" );
+
+console.log(head(["Hello", "Lighthouse", "Labs"]))
