64c5a5c4e921b85543568be711230d1944646331
Idiljei <i.jeilani@gmail.com>
2021-01-05 16:04:24 +0000

    last elements of array

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+};
+ 
+const tail = function(arr) {
+  let newArray = []; 
+  for (let i = 1; i < arr.length; i++) { //starting index at 1 skips the array head in loop  
+    newArray.push(arr[i]);
+  } 
+  return newArray;
+};
+// TEST CODE
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail(["Yo Yo", "Lighthouse", "Labs"]));
