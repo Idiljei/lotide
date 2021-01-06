ef579ab65daf059faa67def03da49f032c352329
Idiljei <i.jeilani@gmail.com>
2021-01-06 22:17:37 +0000

    head of array

--- head.js
+++ head.js
@@ -1,6 +1,16 @@
+// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
+  if ( actual === expected ) {
+   console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`); 
+
+  } else {
+    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+  
 };
+  
+
 const head = function (array) {
 
   return array[0] 
@@ -11,7 +21,7 @@ const head = function (array) {
 
 // TEST CODE
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello" );
 
-console.log(head(["Hello", "Lighthouse", "Labs"]))
+
+assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello" );
+assertEqual(head([5,6,7]), 5);
