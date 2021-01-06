705f92cd6d9839ac575b6477388bfb5ed3b8869f
Idiljei <i.jeilani@gmail.com>
2021-01-06 05:33:32 +0000

    comparing two arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,20 +1,33 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
-  if ( actual === expected ) {
-   console.log(`Assertion Passed: ${actual} === ${expected}`); 
-
-  } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-  }
   
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// Function comparing equality of two arrays 
+const eqArrays = function(arrOne, arrTwo) {
+if (arrOne.length !== arrTwo.length) {
+  return false;
+}
+// create  loops to see the values of the arrays
+for (let i = 0; i < arrOne.length; i++) {
+  if (arrOne[i] !== arrTwo[i]) 
+  return false
+}
+return true;
+}
+    
+   
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
-console.log(assertEqual( 0, 1));
-console.log(assertEqual(0,0));
+// TEST CODE
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
