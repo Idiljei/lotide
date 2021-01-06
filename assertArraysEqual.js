19b7958f91764a14aaafefee1fe8abf46e2b62b1
Idiljei <i.jeilani@gmail.com>
2021-01-06 21:30:13 +0000

    using eqArray function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,16 @@
-//Function comparing equality of two arrays 
-const eqArrays = function(arrOne, arrTwo) {
-if (arrOne.length !== arrTwo.length) {
-  return false;
-}
-// create  loops to see the values of the arrays
-for (let i = 0; i < arrOne.length; i++) {
-  if (arrOne[i] !== arrTwo[i]) 
-  return false
-}
-return true;
-}
+// Function comparing equality of two arrays 
+  const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  // create  loops to see the values of the arrays
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) 
+    return false
+  }
+  return true;
+  }
+    
 
 // Use eqArray Function to pass through your new arrays
 const assertArraysEqual = function(arr1, arr2) {
@@ -26,8 +27,8 @@ const assertArraysEqual = function(arr1, arr2) {
 
 // TEST CODE
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
 
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
