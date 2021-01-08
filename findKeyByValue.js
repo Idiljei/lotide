685c0bbe70ec83a9da5212046a95e825396386e1
Idiljei <i.jeilani@gmail.com>
2021-01-08 15:57:39 +0000

    Function returning obj key based on values

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,8 +1,8 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
-  if ( actual === expected ) {
-   console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`); 
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`);
 
   } else {
     console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
@@ -10,30 +10,36 @@ const assertEqual = function(actual, expected) {
   
 };
 
-    const bestTVShowsByGenre = { 
-      sci_fi: "The Expanse",
-      comedy: "Brooklyn Nine-Nine",
-      drama:  "The Wire"
-    };
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
 
-const findKeyByValue = function (obj, val) {
-  for (let key in obj) { // looping over properties in obj 
-    if (obj[key] === val) { // key is sci_fi and val is The Wire 
-      return key // 
-    } 
+// function returning the key if the values are equal
+const findKeyByValue = function(obj, val) {
+  for (let key in obj) { // looping over properties in obj
+    if (obj[key] === val) { // key is sci_fi and val is The Wire
+      return key; //
+    }
 
-  } 
-
-
-}
+  }
 
 
+};
 
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), undefined);
- assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 
