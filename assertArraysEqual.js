84f1e5acebfdfd0b129b9da99aa910924a012421
Idiljei <i.jeilani@gmail.com>
2021-01-11 22:45:33 +0000

    refactored using modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,34 +1,24 @@
-// Function comparing equality of two arrays 
-  const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  // create  loops to see the values of the arrays
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) 
-    return false
-  }
-  return true;
-  }
-    
+// Function comparing equality of two arrays
+const eqArrays = require('./eqArrays');
 
 // Use eqArray Function to pass through your new arrays
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1,arr2)) {
-  console.log(`This is true indeed: ${arr1} === ${arr2}`);
+    console.log(`✅ This is true indeed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`This is false indeed: ${arr1} !== ${arr2}`);
+    console.log(`❌ This is false indeed: ${arr1} !== ${arr2}`);
 
   }
 
 
-} 
+};
   
 
-// TEST CODE
+// // TEST CODE
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //True
+assertArraysEqual([1, 2, 3], [3, 2, 1]); //False
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); //True
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); //False
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
