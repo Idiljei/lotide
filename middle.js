6f835cb0d7ad29a795157d426d5e045e786a2de1
Idiljei <i.jeilani@gmail.com>
2021-01-11 23:49:06 +0000

    refactoring middle

--- middle.js
+++ middle.js
@@ -1,27 +1,4 @@
-// Function comparing equality of two arrays 
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  // create  loops to see the values of the arrays
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) 
-    return false
-  }
-  return true;
-  }
-    
-
-// Use eqArray Function to pass through your new arrays
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-  console.log(`This is true indeed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`This is false indeed: ${arr1} !== ${arr2}`);
-
-  }
-
-} 
+const assertArraysEqual = require('./assertArraysEqual');
   
 // function should return an array with only the middle element(s)
 const middle = function(middleArray) {
@@ -55,11 +32,9 @@ if (middleArray.length % 2 ==0) { // array is even there is no middle number
 
 
 // TEST CODE
+// assertArraysEqual(middle([1, 2, 3]), [2]); //true
+// assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);//true
+// assertArraysEqual(middle([1, 2, 3, 4]), [2]);//false
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
 
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = middle; 
