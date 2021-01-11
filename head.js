9830d728be45d8e72a04f89a1d5aef1544ee390b
Idiljei <i.jeilani@gmail.com>
2021-01-11 21:34:17 +0000

    head function modularized

--- head.js
+++ head.js
@@ -1,14 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if ( actual === expected ) {
-   console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`); 
-
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual');
+ 
   
 
 const head = function (array) {
@@ -20,8 +12,7 @@ const head = function (array) {
 
 
 // TEST CODE
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello" );
+// assertEqual(head([5,6,7]), 5);
 
-
-
-assertEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello" );
-assertEqual(head([5,6,7]), 5);
+module.exports = head; 
