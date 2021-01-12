8b77cdd4bcd3def6f69d7fe5dd173b94559f665c
Idiljei <i.jeilani@gmail.com>
2021-01-12 03:38:47 +0000

    tset function for eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+
+//EqArrays - comparing two arrays
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),true); // => false
